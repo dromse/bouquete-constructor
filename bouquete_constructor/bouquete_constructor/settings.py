ae784a3c3ca6aa67a597c59922287c193ad0d194
dromse <dromseproducer@gmail.com>
2021-10-20 20:12:46 +0300

    added main templates folder to TEMPLATES

--- bouquete_constructor/bouquete_constructor/settings.py
+++ bouquete_constructor/bouquete_constructor/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +55,10 @@ ROOT_URLCONF = 'bouquete_constructor.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            # Added templates folder to scan
+            os.path.join(BASE_DIR, 'main/templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
