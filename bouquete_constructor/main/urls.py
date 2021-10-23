e1759621916f90d770364a9941ad4ad3d764645a
dromse <dromseproducer@gmail.com>
2021-10-23 22:49:25 +0300

    Added: transition for links

--- bouquete_constructor/main/urls.py
+++ bouquete_constructor/main/urls.py
@@ -4,5 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
+    path('cart', views.about, name='cart'),
+    path('checkout', views.about, name='checkout'),
+    path('payments', views.about, name='payments'),
+    path('contacts', views.about, name='contacts'),
+    path('help', views.about, name='help'),
+    path('login', views.about, name='login'),
+    path('status', views.about, name='status'),
 ]
--- bouquete_constructor/main/views.py
+++ bouquete_constructor/main/views.py
@@ -7,3 +7,31 @@ def index(request):
 
 def about(request):
     return render(request, 'main/about.html') 
+
+
+def cart(request):
+    return render(request, 'main/cart.html') 
+
+
+def checkout(request):
+    return render(request, 'main/checkout.html') 
+
+
+def payments(request):
+    return render(request, 'main/payments.html') 
+
+
+def contacts(request):
+    return render(request, 'main/contacts.html') 
+
+
+def help(request):
+    return render(request, 'main/help.html') 
+
+
+def status(request):
+    return render(request, 'main/status.html')
+
+
+def login(request):
+    return render(request, 'main/help.html')
