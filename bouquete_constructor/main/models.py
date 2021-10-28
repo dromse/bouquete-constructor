9227d072cad5edb259092a0817243b565f7f3fb7
dromse <dromseproducer@gmail.com>
2021-10-28 13:03:06 +0300

    Added: new model Category

--- bouquete_constructor/main/models.py
+++ bouquete_constructor/main/models.py
@@ -4,18 +4,21 @@ from django.db import models
 class Category(models.Model):
 
     name = models.CharField(verbose_name='Name of category')
+    slug = models.SlugField(unique=True)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.name
 
 
 class Product(models.Model):
 
+    category = models.ForeignKey(Category, verbose_name='Category', on_delete=models.CASCADE)
     name = models.CharField(verbose_name='Name', max_length=50)
+    slug = models.SlugField(unique=True)
     amount = models.IntegerField(verbose_name='Amount')
     price = models.DecimalField(verbose_name='Price', max_digits=9, decimal_places=2)
     image = models.ImageField(verbose_name='Image')
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.name
 
